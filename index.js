6b0c16e63acf48408b57d421ec4004e667f663b7
Sebastiaan Deckers <seb@ninja.sg>
2014-07-12 12:10:25 +0800

    Handle rate limit more gracefully Use OAuth client ID & secret Fetch recent issues of each user

--- index.js
+++ index.js
@@ -4,36 +4,51 @@ var fs = require('fs');
 
 var github = new GitHubApi({
   version: '3.0.0',
-  debug: true
+  debug: false
 });
 
-function getUsersByLocation(location, limit) {
-  var users = [];
+if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
+  github.authenticate({
+      type: 'oauth',
+      key: process.env.GITHUB_CLIENT_ID,
+      secret: process.env.GITHUB_CLIENT_SECRET
+  });
+}
+
+function fetch(method, args, limit) {
   return new Promise(function (resolve, reject) {
-    github.search.users({
-      q: 'location:' + location
-    }, function recvUsers(err, res) {
+    var items = [];
+    method(args, function recv(err, res) {
       if (err) {
-        reject(err);
+        if (err.code === 403) {
+          console.log('Rate limited');
+          setTimeout(function () {
+            console.log('Retrying');
+            method(args, recv);
+          }, 60000);
+        } else {
+          reject(err);
+        }
+        return;
       }
-
       res.items
-        .slice(0, limit - users.length)
-        .forEach(function (user) {
-          users.push(user.login);
-          console.log(users.length, user.login);
+        .slice(0, limit - items.length)
+        .forEach(function (item) {
+          items.push(item);
+          console.log(items.length, item);
         });
-
-      if (users.length >= limit || !github.hasNextPage(res)) {
-        resolve(users);
+      if (items.length >= limit || !github.hasNextPage(res)) {
+        resolve(items);
       } else {
-        github.getNextPage(res, recvUsers);
+        github.getNextPage(res, recv);
       }
     });
   });
 }
 
-getUsersByLocation('Singapore', 50)
+fetch(github.search.users, {
+  q: 'location:' + 'Singapore'
+}, 50)
   .then(function (users) {
     console.log('Got:', users);
     fs.writeFile('users.json', JSON.stringify(users, null, 2), function (err) {
@@ -43,6 +58,27 @@ getUsersByLocation('Singapore', 50)
         console.log('JSON saved');
       }
     });
+    return users;
+  })
+  .then(function (users) {
+    return Promise.all(
+      users.map(function (user) {
+        return fetch(github.search.issues, {
+          q: 'type:pr+state:closed+author:' + user
+        }, 5)
+      })
+    );
+  })
+  .then(function (issues) {
+    console.log('Got:', issues);
+    fs.writeFile('issues.json', JSON.stringify(issues, null, 2), function (err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('JSON saved');
+      }
+    });
+    return issues;
   })
   .catch(function (err) {
     console.error(err);
